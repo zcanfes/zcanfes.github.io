3f9cd50e9282008ca0c2a066750fde47807fa5c5
zcanfes <zcanfes@gmail.com>
2021-12-03 23:29:17 +0300

    Updates

---
+++ precache-manifest.7c1e296de7ec4829c44777b5361e1e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3019178eb3ed74b3b4936da207021f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3911d7e21c64e78f65da",
+    "url": "/static/css/main.74a51ed1.chunk.css"
+  },
+  {
+    "revision": "d73bd0d5792be94635da",
+    "url": "/static/js/2.2e1bc95c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.2e1bc95c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3911d7e21c64e78f65da",
+    "url": "/static/js/main.a0d56e6c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bcffea892e5b7738414dc04ef84d39af",
+    "url": "/static/media/resume.bcffea89.pdf"
+  }
+]);
